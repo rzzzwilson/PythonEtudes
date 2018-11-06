602b1b2f64754656982265f92ffec8d2f2e11d4c
rzzzwilson <rzzzwilson@gmail.com>
2018-11-06 16:52:36 +0700

    Added comments

--- From_CLI_to_GUI/From_CLI_to_GUI.2.py
+++ From_CLI_to_GUI/From_CLI_to_GUI.2.py
@@ -2,7 +2,7 @@
 Prompt the user for two integer numbers and print the sum.
 """
 
-# get first integer
+# get the first integer
 while True:
     try:
         int1 = input('Enter the first integer: ')
@@ -11,6 +11,7 @@ while True:
     except ValueError:
         print(f'Sorry, only want integers.  Something like 123.')
 
+# get the second integer
 while True:
     try:
         int2 = input('Enter the second integer: ')
@@ -19,5 +20,6 @@ while True:
     except ValueError:
         print(f'Sorry, only want integers.  Something like 123.')
 
+# calculate the sum and print it
 the_sum = int1 + int2
 print(f'The sum of {int1} and {int2} is {the_sum}')
