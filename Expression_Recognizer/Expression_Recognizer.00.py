bda324dfe20683fae3f3e14915a20c9c237db098
rzzzwilson <rzzzwilson@gmail.com>
2018-11-13 17:55:03 +0700

    Working on Expression Analyzer etude

--- Expression_Recognizer/Expression_Recognizer.00.py
+++ Expression_Recognizer/Expression_Recognizer.00.py
@@ -1,9 +1,22 @@
 """
-Prompt the user for two integer numbers and print the sum.
+Read a simple expression from sys.argv and print the result.
+
+Usage: Expression_Recognizer.00.py <integer> <operator> <integer>
 """
 
-int1 = input('Enter the first integer: ')
-int2 = input('Enter the second integer: ')
-the_sum = int1 + int2
-print(f'The sum of {int1} and {int2} is {the_sum}')
+import sys
 
+args = sys.argv[1:]
+integer1 = int(args[0])
+operator = args[1]
+integer2 = int(args[2])
+
+if operator == '+':
+    result = integer1 + integer2
+elif operator == '-':
+    result = integer1 - integer2
+else:
+    print(f"Unrecognized operator: '{operator}'")
+    sys.exit(1)     # exut here
+
+print(f'Expresssion {integer1} {operator} {integer2} = {result}')
