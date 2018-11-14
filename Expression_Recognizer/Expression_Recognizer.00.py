1858230f46ed26e6c24f4381e93d1aecc17760e2
rzzzwilson <rzzzwilson@gmail.com>
2018-11-14 16:20:15 +0700

    Working on Expression Analyzer etude

--- Expression_Recognizer/Expression_Recognizer.00.py
+++ Expression_Recognizer/Expression_Recognizer.00.py
@@ -1,15 +1,17 @@
 """
-Read a simple expression from sys.argv and print the result.
+Read a simple expression from the user and print the result.
 
-Usage: Expression_Recognizer.00.py <integer> <operator> <integer>
+Recognizesan expression of the form: <integer> <operator> <integer>
+where <operator> may be either '+' or '-'.
 """
 
-import sys
+expression = input('Enter expression: ')
+fields = expression.split()
 
-args = sys.argv[1:]
-integer1 = int(args[0])
-operator = args[1]
-integer2 = int(args[2])
+(integer1, operator, integer2) = fields
+
+integer1 = int(integer1)
+integer2 = int(integer2)
 
 if operator == '+':
     result = integer1 + integer2
