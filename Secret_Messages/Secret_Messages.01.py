b2232096bd1513a85bc3449e80699a0050e6ccf1
rzzzwilson <rzzzwilson@gmail.com>
2019-01-24 16:59:59 +0700

    Updating etude 01 code

---
+++ Secret_Messages/Secret_Messages.01.py
@@ -0,0 +1,32 @@
+"""
+Secret_Messages.00.py
+
+Get pixel data from an image file.
+
+Usage: Secret_Messages.00.py <input file> <output file>
+"""
+
+import sys
+from PIL import Image
+
+# get the input and output filenames and the text message
+if len(sys.argv) != 4:
+    print(f'Sorry, expected two filenames and a text message')
+    sys.exit(1)
+
+input_filename = sys.argv[1]
+output_filename = sys.argv[2]
+text_msg = sys.argv[3]
+
+print(f'input_filename={input_filename}, output_filename={output_filename}, text_msg={text_msg}')
+
+# open input input_filename and get pixel data
+image = Image.open(input_filename)
+pixels = list(image.getdata())
+print(f'pixels={pixels}')
+
+# change the pixel data and write it to the output file
+pixels = [(0, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 0)]
+print(f'NEW pixels={pixels}')
+image.putdata(pixels)
+image.save(output_filename)
