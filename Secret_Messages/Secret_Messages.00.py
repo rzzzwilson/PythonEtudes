eb41d057bade552940776267bae465b1128a9c46
rzzzwilson <rzzzwilson@gmail.com>
2019-01-24 16:59:41 +0700

    Final version

--- Secret_Messages/Secret_Messages.00.py
+++ Secret_Messages/Secret_Messages.00.py
@@ -9,13 +9,16 @@ Usage: Secret_Messages.00.py <input file> <output file>
 import sys
 from PIL import Image
 
-# get the input and output filenames
-if len(sys.argv) != 3:
-    print(f'Sorry, expected two filenames')
+# get the input and output filenames and the text message
+if len(sys.argv) != 4:
+    print(f'Sorry, expected two filenames and a text message')
     sys.exit(1)
 
 input_filename = sys.argv[1]
 output_filename = sys.argv[2]
+text_msg = sys.argv[3]
+
+print(f'input_filename={input_filename}, output_filename={output_filename}, text_msg={text_msg}')
 
 # open input input_filename and get pixel data
 image = Image.open(input_filename)
