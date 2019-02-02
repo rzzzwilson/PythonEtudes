f0995eb9bd00503631ba1a6013f2367a4f073dfb
rzzzwilson <rzzzwilson@gmail.com>
2019-02-02 10:12:19 +0700

    test

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 from PIL import Image
-
-input_image = '4x4.bmp'
-encoded_image = '4x4_encoded.bmp'
-
+ 
 def bits_tostring(data):
     result = ''
     accum = 0
@@ -17,12 +14,13 @@ def bits_tostring(data):
           shift = 0
     return result
  
+ 
 # Now beginning main logic
 mode = input("Would you like to [E]ncrypt or [D]ecrypt? \n")
 mode = mode.upper()     # so user can enter 'e' or 'E', etc
  
 if mode == 'E':
-    image = Image.open(input_image)
+    image = Image.open('two.bmp')
     pixels = list(image.getdata())
  
     plaintext = input("What message would you like to hide? \n")
@@ -30,25 +28,25 @@ if mode == 'E':
  
     for (pix, ch) in zip(pixels, plaintext):
         (r, g, b) = pix
-        new_r = r ^ ord(ch)
+        new_r = r ^ ord(ch)   # need to convert 'ch' to an integer value
         new_pixels.append((new_r, g, b))
-
+ 
     image.putdata(new_pixels)
-    image.save(encoded_image)
+    image.save('two_encoded.bmp')
  
 if mode == 'D':
-    image = Image.open(input_image)
+    image = Image.open('two.bmp')
     pixels = list(image.getdata())
-
-    image_encoded = Image.open(encoded_image)
+ 
+    image_encoded = Image.open('two_encoded.bmp')
     pixels_encoded = list(image_encoded.getdata())
  
     decoded_pixels = []
+ 
     for (pix_plain, pix_encoded) in zip(pixels, pixels_encoded):
         (r_p, g_p, b_p) = pix_plain
         (r_e, g_e, b_e) = pix_encoded
         r_decoded = r_p ^ r_e
         decoded_pixels.append(r_decoded)
-    # at this point "decoded_pixels" contains all N bit pixel values for the complete message
     decoded_bits = bits_tostring(decoded_pixels)  # called only once
     print(decoded_bits)
