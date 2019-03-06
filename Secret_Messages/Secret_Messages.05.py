4242afdf9bde04b840386ca3b64e27a8f74c40e1
rzzzwilson <rzzzwilson@gmail.com>
2019-03-06 11:16:30 +0700

    Rework to make it easier to understand

--- Secret_Messages/Secret_Messages.05.py
+++ Secret_Messages/Secret_Messages.05.py
@@ -6,44 +6,29 @@ Decode a list of N bit values into a text string.
 Usage: Secret_Messages.05.py
 """
 
-number_of_bits = 0      # the number of bits in each value (ie, N)
-accum = 0               # accumulator for N bit values
-shift = 0               # counter of how far to shift left
-result = ''             # place to save each decoded character
+def nbits_to_string(data, num_bits):
+    """Convert a list of Nbit values to a python string.
 
-def bits_init_tostring(numbits):
-    """Initialize the decode system.
+    data      the list of Nbit values
+    num_bits  the number of bits in each value in "data"
 
-    numbits  the number of bits in each decode value (1, 2, 4 or 8)
-
-    Returns the previous decoded string, if any.
+    Returns a simple python string.
     """
 
-    global number_of_bits, accum, shift, result
-
-    old_result = result
-
-    number_of_bits = numbits    # remember the bit value size (ie, N)
-    accum = 0
-    shift = 0
     result = ''
 
-    return old_result           # return the previous string
+    accum = 0                     # initialize state variables
+    shift = 0
 
-def bits_put_tostring(bits):
-    """Accumulate an N bit value.
+    for bits in data:
+        accum += bits << shift    # shift bit value left
+        shift += num_bits         # shift next value "num_bits" places
+        if shift >= 8:            # if we have 8 bits
+            result += chr(accum)  # put new character into result
+            accum = 0             # initialize ready for next character
+            shift = 0
 
-    bits  the N bit value to accumulate
-    """
-
-    global accum, shift, result
-
-    accum += bits << shift
-    shift += number_of_bits
-    if shift >= 8:
-        result += chr(accum)
-        accum = 0
-        shift = 0
+    return result
 
 # test code
 data = [1, 2, 0, 1, 0, 1, 3, 1,    # the 2bit-encoded message
@@ -52,7 +37,5 @@ data = [1, 2, 0, 1, 0, 1, 3, 1,    # the 2bit-encoded message
         3, 2, 2, 1, 3, 0, 3, 1,
         1, 0, 2, 0]
 
-bits_init_tostring(2)
-for val in data:
-    bits_put_tostring(val)
-print(bits_init_tostring(2))
+decoded_data = nbits_to_string(data, 2)
+print(decoded_data)
