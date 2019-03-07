621de6864710eda6e4ef479f2bb66deda5c38896
rzzzwilson <rzzzwilson@gmail.com>
2019-03-07 13:43:05 +0700

    Rework to make it easier to understand

--- Secret_Messages/Secret_Messages.07.1.py
+++ Secret_Messages/Secret_Messages.07.1.py
@@ -3,90 +3,86 @@ Secret_Messages.07.1.py
 
 Decode a secret message given the original and encoded image files.
 
-Usage: Secret_Messages.07.1.py <original image file> <encoded image file>
+Usage: Secret_Messages.07.1.py <original image file> <encoded image file> <number of bits>
 """
 
 import sys
 from PIL import Image
 
-number_of_bits = 0      # the number of bits in each value (ie, N)
-accum = 0               # accumulator for N bit values
-shift = 0               # counter of how far to shift left
-result = []             # place to save each decoded byte
+def nbits_to_string(data, num_bits):
+    """Convert a list of Nbit values to a python bytestring.
 
-def bits_init_tostring(numbits):
-    """Initialize the decode system.
+    data      the list of Nbit values
+    num_bits  the number of bits in each value in "data"
 
-    numbits  the number of bits in each decode value (1, 2, 4 or 8)
-
-    Returns the previous decoded string, if any.
+    Returns a simple python string.
     """
 
-    global number_of_bits, accum, shift, result
-
-    old_result = bytes(result).decode(encoding='utf_8')
-
-    number_of_bits = numbits    # remember the bit value size (ie, N)
-    accum = 0
-    shift = 0
     result = []
 
-    return old_result           # return the previous string
+    accum = 0                     # initialize state variables
+    shift = 0
 
-def bits_put_tostring(bits):
-    """Accumulate an N bit value.
+    for bits in data:
+        accum = accum + (bits << shift)    # shift bit value left
+        shift += num_bits         # shift next value "num_bits" places
+        if shift >= 8:            # if we have accumulated 8 bits
+            if accum == 0:        # a zero byte, quit
+                break
+            result.append(accum)  # put new character into result list
+            accum = 0             # initialize ready for next character
+            shift = 0
 
-    bits  the N bit value to accumulate
+    return bytes(result)
+
+def main(original_filename, encoded_filename, num_bits):
+    """Decode an image into text.
+
+    original_filename  the original image filename
+    encoded_filename   the encoded image filename
+    num_bits           the number of bits in the encoding
+
+    Returns the decoded string.
     """
 
-    global accum, shift, result
+    # open input original_filename and get pixel data
+    original_image = Image.open(original_filename)
+    original_pixels = list(original_image.getdata())
+    
+    # ditto for the encoded file
+    encoded_image = Image.open(encoded_filename)
+    encoded_pixels = list(encoded_image.getdata())
+    
+    # collect the list of Nbit values
+    data = []
+    for (o_pix, e_pix) in zip(original_pixels, encoded_pixels):
+        (o_r, o_g, o_b) = o_pix
+        (e_r, e_g, e_b) = e_pix
 
-    accum += bits << shift
-    shift += number_of_bits
-    if shift >= 8:
-        result.append(accum)
-        accum = 0
-        shift = 0
+        # decode the three pixel values and save in "data" list
+        data.append(o_r ^ e_r)
+        data.append(o_g ^ e_g)
+        data.append(o_b ^ e_b)
+
+    # convert to a list of bytes
+    result = nbits_to_string(data, num_bits)
+
+    # return a string
+    return result.decode(encoding='utf_8')
+
 
 # get the original and encoded filenames
-if len(sys.argv) != 3:
-    print(f'Sorry, expected two filenames')
+if len(sys.argv) != 4:
+    print(f'Sorry, expected two filenames and number of bits')
     sys.exit(1)
 
 original_filename = sys.argv[1]
 encoded_filename = sys.argv[2]
+num_bits = int(sys.argv[3])
 
 print(f'original_filename={original_filename}, encoded_filename={encoded_filename}')
+print(f'num_bits={num_bits}')
 
-# open input original_filename and get pixel data
-original_image = Image.open(original_filename)
-original_pixels = list(original_image.getdata())
-
-# ditto for the encoded file
-encoded_image = Image.open(encoded_filename)
-encoded_pixels = list(encoded_image.getdata())
-
-# decode the text message
-bits_init_tostring(2)
-for (o_pix, e_pix) in zip(original_pixels, encoded_pixels):
-    # handle any fourth pixel data value
-    if len(o_pix) == 3:
-        (o_r, o_g, o_b) = o_pix
-    if len(o_pix) == 4:
-        (o_r, o_g, o_b, _) = o_pix
-
-    if len(e_pix) == 3:
-        (e_r, e_g, e_b) = e_pix
-    if len(e_pix) == 4:
-        (e_r, e_g, e_b, _) = e_pix
-
-    # decode the three pixel values
-    bits_r = o_r ^ e_r
-    bits_put_tostring(bits_r)
-    bits_g = o_g ^ e_g
-    bits_put_tostring(bits_g)
-    bits_b = o_b ^ e_b
-    bits_put_tostring(bits_b)
-
-result = bits_init_tostring(2)
+result = main(original_filename, encoded_filename, num_bits)
 print(result)
+
