7dfe66ed6d656c052d792e663cd2cfa8bdd648b6
rzzzwilson <rzzzwilson@gmail.com>
2019-03-07 10:50:41 +0700

    Rework to make it easier to understand

--- Secret_Messages/Secret_Messages.06.py
+++ Secret_Messages/Secret_Messages.06.py
@@ -3,82 +3,71 @@ Secret_Messages.06.py
 
 Decode a secret message given the original and encoded image files.
 
-Usage: Secret_Messages.06.py <original image file> <encoded image file>
+Usage: Secret_Messages.06.py <original image file> <encoded image file> <number of bits>
 """
 
 import sys
 from PIL import Image
 
-number_of_bits = 0      # the number of bits in each value (ie, N)
-accum = 0               # accumulator for N bit values
-shift = 0               # counter of how far to shift left
-result = ''             # place to save each decoded character
+def nbits_to_string(data, num_bits):
+    """Convert a list of Nbit values to a python string.
 
-def bits_init_tostring(numbits):
-    """Initialize the decode system.
+    data      the list of Nbit values
+    num_bits  the number of bits in each value in "data"
 
-    numbits  the number of bits in each decode value (1, 2, 4 or 8)
-
-    Returns the previous decoded string, if any.
+    Returns a simple python string.
     """
 
-    global number_of_bits, accum, shift, result
-
-    old_result = result
-
-    number_of_bits = numbits    # remember the bit value size (ie, N)
-    accum = 0
-    shift = 0
     result = ''
 
-    return old_result           # return the previous string
+    accum = 0                     # initialize state variables
+    shift = 0
 
-def bits_put_tostring(bits):
-    """Accumulate an N bit value.
+    for bits in data:
+        accum = accum + (bits << shift)    # shift bit value left
+        shift += num_bits         # shift next value "num_bits" places
+        if shift >= 8:            # if we have accumulated 8 bits
+            if accum == 0:        # a zero byte, quit
+                break
+            result += chr(accum)  # put new character into result
+            accum = 0             # initialize ready for next character
+            shift = 0
 
-    bits  the N bit value to accumulate
-    """
+    return result
 
-    global accum, shift, result
+def main(original_filename, encoded_filename, num_bits):
+    # open input original_filename and get pixel data
+    original_image = Image.open(original_filename)
+    original_pixels = list(original_image.getdata())
+    
+    # ditto for the encoded file
+    encoded_image = Image.open(encoded_filename)
+    encoded_pixels = list(encoded_image.getdata())
+    
+    # get list of decoded Nbit values
+    nbit_values = []
+    for (o_pix, e_pix) in zip(original_pixels, encoded_pixels):
+        # unpack each pixel tuple
+        (o_r, o_g, o_b) = o_pix
+        (e_r, e_g, e_b) = e_pix
+    
+        # decode the three pixel values, append to Nbit value list
+        nbit_values.append(o_r ^ e_r)
+        nbit_values.append(o_g ^ e_g)
+        nbit_values.append(o_b ^ e_b)
 
-    accum += bits << shift
-    shift += number_of_bits
-    if shift >= 8:
-        result += chr(accum)
-        accum = 0
-        shift = 0
+    return nbits_to_string(nbit_values, num_bits)
 
 # get the original and encoded filenames
-if len(sys.argv) != 3:
-    print(f'Sorry, expected two filenames')
+if len(sys.argv) != 4:
+    print(f'Sorry, expected a number of bits and two filenames')
     sys.exit(1)
 
 original_filename = sys.argv[1]
 encoded_filename = sys.argv[2]
+num_bits = int(sys.argv[3])
 
-print(f'original_filename={original_filename}, encoded_filename={encoded_filename}')
+print(f'original_filename={original_filename}, encoded_filename={encoded_filename}, num_bits={num_bits}')
 
-# open input original_filename and get pixel data
-original_image = Image.open(original_filename)
-original_pixels = list(original_image.getdata())
-
-# ditto for the encoded file
-encoded_image = Image.open(encoded_filename)
-encoded_pixels = list(encoded_image.getdata())
-
-# decode the text message
-bits_init_tostring(2)
-for (o_pix, e_pix) in zip(original_pixels, encoded_pixels):
-    # unpack each pixel tuple
-    (o_r, o_g, o_b) = o_pix
-    (e_r, e_g, e_b) = e_pix
-
-    # decode the three pixel values
-    bits_r = o_r ^ e_r
-    bits_put_tostring(bits_r)
-    bits_g = o_g ^ e_g
-    bits_put_tostring(bits_g)
-    bits_b = o_b ^ e_b
-    bits_put_tostring(bits_b)
-
-print(bits_init_tostring(2))
+result = main(original_filename, encoded_filename, num_bits)
+print(result)
