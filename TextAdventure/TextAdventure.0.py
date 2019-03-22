69b5b9ca2df7400142e3b8d245d0f9c34fb747e5
rzzzwilson <rzzzwilson@gmail.com>
2019-03-22 20:20:35 +0700

    Extending the etude

--- TextAdventure/TextAdventure.0.py
+++ TextAdventure/TextAdventure.0.py
@@ -14,11 +14,9 @@ Some features:
 class Place:
     """A Place in the text adventure."""
 
-    def __init__(self, name, description, long_description,
-                 connections):
+    def __init__(self, name, description, connections):
         self.name = name
         self.description = description
-        self.long_description = long_description
         self.connections = connections
 
     def __str__(self):
@@ -29,25 +27,20 @@ class Place:
 
 # the Places in this adventure
 white_house = Place('white_house', 'at the White house.',
-                    'at the White house, a decaying white weatherboard house.  '
-                    'There are paths to the east and south.',
                     connections = {'east': 'path',
                                    'south': 'forest'})
 
 path = Place('path', 'on a narrow path.',
-             'on a narrow path.  The path runs east-west.',
              connections={'east': 'glade',
                           'west': 'white_house'})
 
 glade = Place('glade', 'in a shadowed glade.',
-              'in a shadowed glade, with exits to the west and southwest.',
               connections = {'west': 'path',
                              'southwest': 'forest'})
 
 forest = Place('forest', 'in a dark difficult forest.',
-               'in a dark difficult forest, with paths leading north and northeast.',
-        connections={'northeast': 'glade',
-                     'north': 'white_house'})
+               connections={'northeast': 'glade',
+                            'north': 'white_house'})
 
 # a dictionary to map place "names" to the actual Place() object
 # solves the "forward reference" problem
@@ -59,7 +52,6 @@ name_place = {'white_house': white_house,
              }
 
 current_place = white_house
-previous_place = None
 
 allowed_moves = {'north': 'north', 'n': 'north',
                  'northeast': 'northeast', 'ne': 'northeast',
@@ -75,30 +67,28 @@ def describe_place(place):
     """Describe the current place.
     
     place  a reference to the Place() object to describe
-
-    If we have just come from this place, print a short description.
     """
 
-    if place != previous_place:
-        print('You are ' + place.long_description)
-    else:
-        print('You are ' + place.description)
-
+    print('You are ' + place.description)
 
 def get_move():
     """Get a legal move command.
-    
+   
+    Accepts any of the canonical moves even if it won't work
+    in the current Place.
+
     Returns the canonical direction string.
     """
 
     while True:
         move = input('Which way? ').lower()
-        if move in allowed_moves:
+        try:
             return allowed_moves[move]
-        print(f"Sorry, '{move}' isn't a legal move here.  Try again.\n")
+        except KeyError:
+            print(f"Sorry, '{move}' isn't a legal move here.  Try again.\n")
 
 def make_move(move):
-    """Check if the move is legal.  If so, move there.
+    """Check if the move is legal in the current Place.  If so, move there.
 
     move  the canonical move string
 
@@ -107,19 +97,16 @@ def make_move(move):
     """
 
     global current_place
-    global previous_place
 
     if move in current_place.connections:
-        new_place = name_place[current_place.connections[move]]
-        describe_place(new_place)
-        (previous_place, current_place) = (current_place, new_place)
+        current_place = name_place[current_place.connections[move]]
         return True
     return False
 
 
 # play the game
-describe_place(current_place)
 while True:
+    describe_place(current_place)
     move = get_move()
     if not make_move(move):
-        print("Sorry, that isn't a legal move here.  Try again.\n")
+        print("Sorry, you can't move in that direction.  Try again.\n")
